6e05e593391aba3aa4b828c56975935daaf7c213
Stark <386707933@qq.com>
2019-04-15 15:01:38 +0800

    add javadoc

--- src/main/java/com/stark/service/HelloService.java
+++ src/main/java/com/stark/service/HelloService.java
@@ -1,13 +1,34 @@
 package com.stark.service;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * To test api doc
+ *
+ * @author StarkTan
+ * @since 1.0
+ */
 @Service
 public class HelloService {
+    /**
+     * test javadoc api
+     *
+     * @return test string
+     * @since 1.0
+     */
     public String hello() {
-        return "Hello 2019-4-1 123123123:00";
+        return "Hello ";
     }
 
+    /**
+     * test javadoc api
+     *
+     * @return test string
+     * @since 1.0
+     */
     public String noCover() {
         return "no cover";
     }
